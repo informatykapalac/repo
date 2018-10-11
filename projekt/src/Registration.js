11820ee4dff5a6eb9906c17b5e2055441e5503a1
Szymon <informatykapalac@gmail.com>
2018-10-11 19:37:57 +0200

    rejestracja pola tekstowe

--- projekt/src/Registration.js
+++ projekt/src/Registration.js
@@ -13,12 +13,26 @@ class Example extends React.Component {
   }
   render() {
 	return (
-		<Form>
-			<FormGroup>
-				<Label for="exampleEmail">Email</Label>
-				<Input type="email" name="email" id="exampleEmail" placeholder="Wpisz swój e-mail" value={this.state.email} />
-			</FormGroup>
-		</Form>
+		<div className="rejestrTlo">
+			<div className="rejestrMain">
+				<Form>
+					<div className="rejestrContent">
+						<FormGroup>
+							<Label for="exampleNick">Nick</Label>
+							<Input type="textarea" name="nick" id="exampleNick" placeholder="Wpisz swój nick"  />
+						</FormGroup>
+						<FormGroup>
+							<Label for="exampleEmail">Email</Label>
+							<Input type="email" name="email" id="exampleEmail" placeholder="Wpisz swój e-mail"  />
+						</FormGroup>
+						<FormGroup>
+							<Label for="examplePass">Hasło</Label>
+							<Input type="password" name="pass" id="examplePass" placeholder="Wpisz swoje hasło"  />
+						</FormGroup>
+					</div>
+				</Form>
+			</div>
+		</div>
 	);
   }
 }
