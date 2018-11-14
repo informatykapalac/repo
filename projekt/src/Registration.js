f4e5542a8fd7670914101acd69b0d58f04cc7933
Szymon <devoxiyt@gmail.com>
2018-11-14 18:59:57 +0100

    prawie dziala rejestracja

--- projekt/src/Registration.js
+++ projekt/src/Registration.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { Container, Jumbotron, Col, Row, Button, Form, FormGroup, Input } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import ErrBox from "./ErrBox";
 
 class Example extends React.Component {
   constructor() {
@@ -9,17 +10,35 @@ class Example extends React.Component {
 	this.state = {
 		nick: "",
 		email: "",
-		pass: ""
+		pass: "",
+		error: ""
 	};
 
 	this.handleReg = this.handleReg.bind(this);
-  this.handleChange = this.handleChange.bind(this);
+	this.handleChange = this.handleChange.bind(this);
   }
+  checkData(user, pass){
+		const userRegex = new RegExp(/^[\w]{1,20}$/);
+		const passRegex = new RegExp(/^[\w]{8,30}$/);
+		const bigRegex = new RegExp(/[A-Z]/)
+        if(userRegex.test(user)){
+            if(passRegex.test(pass) && bigRegex.test(pass)){
+                return true;
+			}
+			else{
+				this.setState({error: "Hasło musi mieć od 8 do 30 znaków i dużą literę."});
+				return false;
+			}
+		}else{
+			this.setState({error: "Nazwa użytkownika musi mieć od 1 do 20 znaków."});
+			return false;
+		}
+	}
 
   handleReg(event) {
 
     event.preventDefault();
-
+	if(this.checkData(this.state.username, this.state.password)){
     const data = {
       name: this.state.nick,
       email: this.state.email,
@@ -29,6 +48,10 @@ class Example extends React.Component {
     axios.post('/register', { data }).then(res => {
       console.log("DONE");
     })
+	}
+	else {
+		console.log(this.state.error);
+	}
   }
 
   handleChange(event) {
@@ -42,22 +65,29 @@ class Example extends React.Component {
 				<Row>
 					<Col xs="12" md={{size:8, offset:2}} xl={{size:4, offset:4}} className="LoginCol">
 						<Jumbotron color="primary">
-							<h1 className="display-8"><div className="text-center">Zarejestruj się</div></h1>
+							<h1 className="display-8">
+								<div className="text-center">
+									Zarejestruj się
+								</div>
+							</h1>
 								<Form onSubmit={this.handleReg}>
 								    <FormGroup>
 										<Input type="email" name="email" id="exampleEmail" placeholder="Wpisz e-mail" value={this.state.email} onChange={this.handleChange} />
 									</FormGroup>
-									<FormGroup>
-										<Input type="text" name="nick" id="exampleNick" placeholder="Wpisz nick" value={this.state.nick} onChange={this.handleChange} />
+									<FormGroup onSubmit={this.sendData}>
+										<FormGroup>
+											<Input type="text" name="nick" id="exampleNick" placeholder="Wpisz nick" value={this.state.nick} onChange={this.handleChange} />
+										</FormGroup>
+										<FormGroup>
+											<Input type="password" name="pass" id="examplePass" placeholder="Wpisz hasło" value={this.state.pass} onChange={this.handleChange} />
+										</FormGroup>
+										<FormGroup>
+											<Input type="password" name="2pass" id="example2Pass" placeholder="Powtórz hasło" />
+										</FormGroup>
 									</FormGroup>
+									<ErrBox error={this.state.error}/>
 									<FormGroup>
-										<Input type="password" name="pass" id="examplePass" placeholder="Wpisz hasło" value={this.state.pass} onChange={this.handleChange} />
-									</FormGroup>
-									<FormGroup>
-										<Input type="password" name="2pass" id="example2Pass" placeholder="Powtórz hasło" />
-									</FormGroup>
-									<FormGroup>
-									<Button id="zarejestruj" type="submit" color="warning" block>Zarejestruj się!</Button>{' '}
+										<Button id="zarejestruj" type="submit" color="warning" block>Zarejestruj się!</Button>{' '}
 									</FormGroup>
 									<Link to="/login"><Button id="mamjuzkonto" color="secondary" block>Mam już konto</Button></Link>{' '}
 								</Form>
