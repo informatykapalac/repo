2d484385a2806e6c4305d5cfdeb7c1f7254be8bf
Wiktor Jurkiewicz <wiktorjurkiewicz@mac-book-pro.local>
2018-12-17 22:52:52 +0100

    poprawki Layer2

--- projekt/src/Layer2Component.js
+++ projekt/src/Layer2Component.js
@@ -27,8 +27,7 @@ class Layer_2 extends Component {
   componentDidMount() {
     const LgraphicList = [];
     Layer2_config.map((img_props, i)=>{
-        const size = img_props.img_size
-        const img = new window.Image(size,size);
+        const img = new window.Image();
         img.src = '/layer2/' + img_props.img_src;
         LgraphicList[i] = img;
         if(i===Layer2_config.length - 1) {
@@ -37,19 +36,19 @@ class Layer_2 extends Component {
           this.setState({GraphicPos:[
             {
               id: "0",
-              img_nr:0,
+              img_nr:21,
               img_x:1000,
               img_y:1000
             },
             {
               id: "1",
-              img_nr:0,
+              img_nr:22,
               img_x:1100,
               img_y:1000
             },
             {
               id: "2",
-              img_nr:9,
+              img_nr:23,
               img_x:1050,
               img_y:1000
             }
@@ -68,7 +67,6 @@ class Layer_2 extends Component {
         {
           this.state.GraphicPos.map((Graphic_Props)=>{
             const Graphic = this.state.GraphicsList[Graphic_Props.img_nr];
-            console.log(Graphic_Props)
 
             if(Graphic_Props.img_nr == 0 || Graphic_Props.img_nr == 1 || Graphic_Props.img_nr == 2 || Graphic_Props.img_nr == 3 || Graphic_Props.img_nr == 4 || Graphic_Props.img_nr == 5) {
               const id = Graphic_Props.id;
@@ -88,8 +86,8 @@ class Layer_2 extends Component {
               <Image
                 image = {Graphic}
                 key = {uuidv4()}
-                width={Graphic.width * this.props.avgZoom}
-                height={Graphic.height * this.props.avgZoom}
+                width={32 * this.props.avgZoom}
+                height={32 * this.props.avgZoom}
                 x={(Graphic_Props.img_x + this.props.mapPos.x) * this.props.avgZoom}
                 y={(Graphic_Props.img_y + this.props.mapPos.y) * this.props.avgZoom}
             />
