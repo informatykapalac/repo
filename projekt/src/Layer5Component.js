352831f0fc12fa5c4b7cdb216cb4366091e53ea5
_jrw <_jrw@pc104.home>
2018-12-18 22:41:29 +0100

    Naprawa Redux

--- projekt/src/Layer5Component.js
+++ projekt/src/Layer5Component.js
@@ -26,27 +26,27 @@ class Layer_5 extends Component {
 	componentDidMount() {
 		console.log("mleko")
 		const LgraphicList = [];
-    	Layer5_config.map((img_props, i)=>{
+    Layer5_config.map((img_props, i) => {
         const img = new window.Image();
         img.src = '/layer5/' + img_props.img_src;
-		LgraphicList[i] = img;
+				LgraphicList[i] = img;
         if(i==Layer5_config.length - 1) {
-        img.onload = () =>{
-          this.setState({GraphicsList: LgraphicList});
-          	this.setState({GraphicPos:[
-				{
-					id: "0",
-					img_nr:0,
-					img_x:0,
-					img_y:0
-				  },
-          ]
-		})
-      }
-     }
+        	img.onload = () => {
+          	this.setState({ GraphicsList: LgraphicList });
+          	this.setState({ GraphicPos: [
+							{
+								id: "0",
+								img_nr:0,
+								img_x:0,
+								img_y:0
+				  		},
+          	]
+					});
+      	}
+     	}
     })
 	}
-	
+
 
 	render(){
 		return(
@@ -54,7 +54,8 @@ class Layer_5 extends Component {
 			{
             this.state.GraphicPos.map((Graphic_Props)=>{
 			const Graphic = this.state.GraphicsList[Graphic_Props.img_nr];
-			if(Graphic_Props.img_nr == 0) { 
+ 
+			if(Graphic_Props.img_nr == 0) {
 				 this.state.wi = 1280 * (this.props.screenSize.w / 1280)
 				 this.state.hi = 720 * (this.props.screenSize.h / 720)
 			}	else {
